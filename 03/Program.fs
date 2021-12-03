152fd7bb3babc585bb82e2dab0f2f43620bf0a85
Niklas Mollenhauer <nikeee@outlook.com>
2021-12-03 09:14:53 +0100

    Solve 3: 2/2

--- 03/Program.fs
+++ 03/Program.fs
@@ -18,11 +18,22 @@ let compareBits (input : seq<string>) (bitIndex : int) : int =
     |> Seq.map (fun line -> line.[bitIndex])
     |> Seq.sumBy (fun bit -> if bit = '1' then 1 else -1)
 
-let mapBits (bitCount : int) (input : seq<string>) (mapperFn : int -> string) : string =
+let mapBitsPart1 (bitCount : int) (input : seq<string>) (mapperFn : int -> string) : string =
     seq {0 .. (bitCount - 1)}
     |> Seq.map (compareBits input)
-    |> Seq.map (mapperFn)
+    |> Seq.map mapperFn
     |> String.concat ""
+
+let rec filterBitsByConsecutiveMostFrequent (candidates : Set<string>) (currentBit : int) (getRelevantBit : int -> char) =
+    let bitComparison = compareBits candidates currentBit
+    let mostFrequentBit = getRelevantBit bitComparison
+
+    let nextValidCandidates = candidates
+                                |> Seq.filter (fun c -> c.[currentBit] = mostFrequentBit)
+                                |> Set.ofSeq
+    if nextValidCandidates.Count = 1
+    then nextValidCandidates |> Seq.head
+    else filterBitsByConsecutiveMostFrequent nextValidCandidates (currentBit + 1) getRelevantBit
 
 [<EntryPoint>]
 let main _ =
@@ -31,7 +42,11 @@ let main _ =
                     |> Seq.map String.length
                     |> Seq.max
 
-    let gamma = mapBits bitCount input (fun r -> if r >= 0 then "1" else "0") |> parseBinary
-    let epsilon = mapBits bitCount input (fun r -> if r >= 0 then "0" else "1") |> parseBinary
+    let gamma = mapBitsPart1 bitCount input (fun r -> if r >= 0 then "1" else "0") |> parseBinary
+    let epsilon = mapBitsPart1 bitCount input (fun r -> if r >= 0 then "0" else "1") |> parseBinary
     printfn "Power consumption; Part 1: %d" (gamma * epsilon)
+
+    let oxygen = filterBitsByConsecutiveMostFrequent input 0 (fun cmp -> if cmp >= 0 then '1' else '0') |> parseBinary
+    let co2 = filterBitsByConsecutiveMostFrequent input 0 (fun cmp -> if cmp >= 0 then '0' else '1') |> parseBinary
+    printfn "Life Support Rating; Part 2: %d" (oxygen * co2)
     0
