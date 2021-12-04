afc457f7a99831bf2b12c8b71b62e9d2c9c6b572
Niklas Mollenhauer <nikeee@outlook.com>
2021-12-04 15:00:16 +0100

    Solve 4: 2/2

--- 04/main.c
+++ 04/main.c
@@ -21,18 +21,6 @@ void mark_number_as_drawn(number *board, number n)
             board[i] = -1;
 }
 
-void print_board(number *board)
-{
-    for (size_t x = 0; x < BOARD_SIZE; ++x)
-    {
-        for (size_t y = 0; y < BOARD_SIZE; ++y)
-        {
-            printf("%3" SCNi8, board[x * BOARD_SIZE + y]);
-        }
-        printf("\n");
-    }
-}
-
 /**
  * This function is full of UB :)
  */
@@ -79,16 +67,24 @@ uint32_t compute_board_score(number *board, number last_drawn_number)
 
 int main()
 {
+    int exit_code = EXIT_SUCCESS;
+
     number *drawn_numbers = calloc(100, sizeof(number));
     if (drawn_numbers == NULL)
-        return -1;
+    {
+        exit_code = EXIT_FAILURE;
+        goto fail_0;
+    }
 
     size_t drawn_numbers_count = 0;
     do
     {
         int read = scanf("%" SCNi8, &drawn_numbers[drawn_numbers_count]);
         if (read != 1)
-            return -1;
+        {
+            exit_code = EXIT_FAILURE;
+            goto fail_1;
+        }
 
         ++drawn_numbers_count;
     } while (getchar() == ',');
@@ -101,7 +97,10 @@ int main()
     {
         number *current_board = calloc(BOARD_NUMBER_COUNT, sizeof(number));
         if (current_board == NULL)
-            return -1;
+        {
+            exit_code = EXIT_FAILURE;
+            goto fail_2;
+        }
 
         for (size_t i = 0; i < BOARD_NUMBER_COUNT; ++i)
         {
@@ -124,6 +123,8 @@ int main()
         }
     }
 
+    size_t finished_boards = 0;
+
     for (size_t number_index = 0; number_index < drawn_numbers_count; ++number_index)
     {
         number n = drawn_numbers[number_index];
@@ -131,23 +132,43 @@ int main()
         for (size_t board_index = 0; board_index < boards_count; ++board_index)
         {
             number *board = boards[board_index];
+            if (board == NULL)
+                continue;
 
             mark_number_as_drawn(board, n);
 
             // Small micro optimization: We can only have a winning board if there were at least BOARD_SIZE numbers drawn
             if (number_index >= BOARD_SIZE && board_has_won(board))
             {
-                uint32_t score = compute_board_score(board, n);
-                printf("Score of winning board; Part 1: %d\n", score);
+                ++finished_boards;
 
-                return 0;
+                if (finished_boards == 1)
+                {
+                    uint32_t score = compute_board_score(board, n);
+                    printf("Score of first winning board; Part 1: %d\n", score);
+                }
+                else if (finished_boards == boards_count)
+                {
+                    uint32_t score = compute_board_score(board, n);
+                    printf("Score of last winning board; Part 2: %d\n", score);
+                }
+
+                free(boards[board_index]);
+
+                // We set the pointer to finished boards to zero, so we can skip them in later checks
+                boards[board_index] = NULL;
             }
         }
     }
 
-    // We don't free memory created with calloc(). We only have a single run and the OS frees up the memory after process termination anyways.
-    // for (size_t i = 0; i < boards_count; ++i)
-    //     free(boards[boards_count]);
-    // free(drawn_numbers);
-    return 0;
+fail_2:
+    for (size_t i = 0; i < boards_count; ++i)
+        if (boards[boards_count] != NULL)
+            free(boards[boards_count]);
+
+fail_1:
+    free(drawn_numbers);
+
+fail_0:
+    return exit_code;
 }
