0e9b7fde276d8ef4d8af0221fd56d58955e04ad3
Niklas Mollenhauer <nikeee@outlook.com>
2021-12-06 18:04:51 +0100

    Solve 6: 2/2

--- 06/Main.scala
+++ 06/Main.scala
@@ -10,23 +10,27 @@ object Day05 extends App {
 
     val ageMap = fish
         .groupBy(identity)
-        .view.mapValues(_.size)
+        .view.mapValues(_.size.toLong)
         .toMap
 
     val part1Distribution = (1 to 80).foldLeft(ageMap)((state, _) => processDay(state))
     val part1Solution = computeResult(part1Distribution)
     println(s"Number of fish after 80 days; Part 1: $part1Solution")
 
+    val part2Distribution = (1 to 256).foldLeft(ageMap)((state, _) => processDay(state))
+    val part2Solution = computeResult(part2Distribution)
+    println(s"Number of fish after 256 days; Part 2: $part2Solution")
 
-    def computeResult(currentData: Map[Int, Int]) : Int = currentData.values.foldLeft(0)(_ + _)
 
-    def processDay(currentData: Map[Int, Int]) : Map[Int, Int] = {
+    def computeResult(currentData: Map[Int, Long]) : Long = currentData.values.foldLeft(0L)(_ + _)
+
+    def processDay(currentData: Map[Int, Long]) : Map[Int, Long] = {
         val nextDataWithoutOverflow = (8 to 1 by -1)
-                        .map(age => (age - 1 -> currentData.getOrElse(age, 0)))
+                        .map(age => (age - 1 -> currentData.getOrElse(age, 0L)))
                         .toMap
 
-        val overflowCount = currentData.getOrElse(0, 0)
-        val seven = currentData.getOrElse(7, 0)
+        val overflowCount = currentData.getOrElse(0, 0L)
+        val seven = currentData.getOrElse(7, 0L)
 
         nextDataWithoutOverflow ++ Map(
             (8 -> overflowCount),
