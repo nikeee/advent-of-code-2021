ee56d2b26b90626d3f533acb97f28914ce2d0272
Niklas Mollenhauer <nikeee@outlook.com>
2021-12-11 00:09:38 +0100

    Solve 10: 2/2

--- 10/main.cr
+++ 10/main.cr
@@ -30,12 +30,12 @@ def get_corrupt_char(line : String)
     if SYNTAX_SCORE_MAP.keys.includes?(next_char)
 
       if stack.size == 0
-        return next_char
+        return {next_char, stack}
       end
 
       opening_char = stack.pop()
       if !PAIRS.includes?({opening_char, next_char})
-        return next_char
+        return {next_char, stack}
       end
 
     else
@@ -44,13 +44,35 @@ def get_corrupt_char(line : String)
 
     offset += 1
   end
-  return nil
+  return {nil, stack}
 end
 
 syntax_score = lines
   .map {|line| get_corrupt_char(line)}
+  .map {|c| c[0]}
   .select {|c| c != nil}
   .map {|c| SYNTAX_SCORE_MAP[c]}
   .sum(0)
 
 puts "Syntax error score; Part 1: #{syntax_score}"
+
+def median(arr)
+  sorted_array = arr.sort
+  center = (sorted_array.size / 2).round.to_i
+  return sorted_array[center]
+end
+
+COMPLETION_SCORE = {
+  '(' => 1,
+  '[' => 2,
+  '{' => 3,
+  '<' => 4,
+}
+
+completion_scores = lines
+  .map {|line| get_corrupt_char(line)}
+  .select {|c| c[0] == nil}
+  .map {|c| c[1].reverse }
+  .map {|c| c.reduce(Int64.new(0)) { |acc, i| (acc * 5) + COMPLETION_SCORE[i] }}
+
+puts "Syntax completion score; Part 1: #{median(completion_scores)}"
