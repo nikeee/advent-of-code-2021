c62e4edd9ad9cd1512621cc91044b689af9cea07
Niklas Mollenhauer <nikeee@outlook.com>
2021-12-18 15:50:15 +0100

    Solve 17: 2/2

--- 17/main.d
+++ 17/main.d
@@ -8,10 +8,10 @@ import std.conv;
 import std.stdio;
 import std.string;
 import std.container.rbtree;
-import std.typecons : tuple;
+import std.typecons : tuple, Tuple;
 import std.algorithm.comparison : min, max;
 
-alias Vector = tuple!("x", "y");
+alias Vector = Tuple!(int, "x", int, "y");
 
 void main() {
 	string input = strip(stdin.readln()); // "target area: x=20..30, y=-10..-5"
@@ -35,33 +35,35 @@ void main() {
 	auto acceleration = Vector(-1, -1);
 
 	// A Red Black tree with the highest element at front
+	// we could also just use a simple int to hold the highest value, but we want to try out an RB tree :)
 	auto highest_ys = redBlackTree!("a > b", int);
+	int hitting_velocities_count = 0;
 
 	// There might be more efficient solutions for this problem, but we take the simple approach and just check some initial conditions
-	foreach (initial_velocity_x; 0..100) {
+	foreach (initial_velocity_x; 0..(max(ta_x0, ta_x1) + 1)) {
 		foreach (initial_velocity_y; -1000..1000) {
+			auto initial_velocity = Vector(initial_velocity_x, initial_velocity_y);
 
 			int highest_y = -1;
 			auto pos = Vector(0, 0);
-			auto velocity = Vector(initial_velocity_x, initial_velocity_y);
+			auto velocity = initial_velocity;
 
 			while (true) {
-				if (pos.x < ta_start.x && velocity.x == 0) {
-					// Not moving in x direction any more, cannot reach target in any way
-					break;
-				}
-
 				if (pos.y > highest_y) {
 					highest_y = pos.y;
 				}
 				if (pos.x >= ta_start.x && pos.x <= ta_end.x && pos.y >= ta_start.y && pos.y <= ta_end.y) {
 					// target area was hit, add highest_y to list and finish check
-					highest_ys.insert([highest_y]);
-					break;
-				}
+					highest_ys.insert(highest_y);
 
-				if (pos.x > ta_end.x || pos.y < ta_end.y) {
+					// every initial velocity gets tested only once, so we don't need a set
+					hitting_velocities_count++;
+					break;
+				} else if (pos.x > ta_end.x || pos.y < ta_start.y) {
 					// target missed
+					break;
+				} else if (pos.x < ta_start.x && velocity.x == 0) {
+					// Not moving in x direction any more, cannot reach target in any way
 					break;
 				}
 
@@ -76,4 +78,5 @@ void main() {
 	}
 
 	writefln("Highest y coordinate of paths that hit the target; Part 1: %d", highest_ys.front);
+	writefln("Number of initial velocities hitting the target; Part 2: %d", hitting_velocities_count);
 }
