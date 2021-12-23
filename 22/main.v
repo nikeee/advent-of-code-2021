5d707a9e765dda19100e33e6138791d76cc0ef76
Niklas Mollenhauer <nikeee@outlook.com>
2021-12-23 02:47:37 +0100

    Solve 22: 2/2

--- 22/main.v
+++ 22/main.v
@@ -8,14 +8,13 @@
 
 import os
 import arrays
-import math.util { imin, imax }
 
 struct Range {
-	start int
-	end int
+	start i64
+	end i64
 }
 
-fn (this Range) size() int {
+fn (this Range) size() i64 {
 	return this.end - this.start + 1
 }
 
@@ -25,7 +24,7 @@ struct Cuboid {
 	y Range
 	z Range
 }
-fn (this Cuboid) volume() int {
+fn (this Cuboid) volume() i64 {
 	return this.x.size() * this.y.size() * this.z.size()
 }
 
@@ -38,10 +37,17 @@ fn (this Cuboid) intersects_with(other Cuboid) bool {
 fn (this Cuboid) intersection(other Cuboid, factor int) Cuboid {
 	return Cuboid {
 		factor,
-		Range { imax(this.x.start, other.x.start), imin(this.x.end, other.x.end) },
-		Range { imax(this.y.start, other.y.start), imin(this.y.end, other.y.end) },
-		Range { imax(this.z.start, other.z.start), imin(this.z.end, other.z.end) },
+		Range { i64max(this.x.start, other.x.start), i64min(this.x.end, other.x.end) },
+		Range { i64max(this.y.start, other.y.start), i64min(this.y.end, other.y.end) },
+		Range { i64max(this.z.start, other.z.start), i64min(this.z.end, other.z.end) },
 	}
+}
+
+fn i64max(a i64, b i64) i64 {
+	return if a > b { a } else { b }
+}
+fn i64min(a i64, b i64) i64 {
+	return if a < b { a } else { b }
 }
 
 fn parse_cuboid(line string) Cuboid {
@@ -51,9 +57,9 @@ fn parse_cuboid(line string) Cuboid {
 	yr := xyz[1].split('=')[1]
 	zr := xyz[2].split('=')[1]
 
-	xs := xr.split('..').map(it.int())
-	ys := yr.split('..').map(it.int())
-	zs := zr.split('..').map(it.int())
+	xs := xr.split('..').map(it.i64())
+	ys := yr.split('..').map(it.i64())
+	zs := zr.split('..').map(it.i64())
 
 	return Cuboid {
 		if split[0] == 'on' { 1 } else { -1 },
@@ -63,11 +69,7 @@ fn parse_cuboid(line string) Cuboid {
 	}
 }
 
-
-fn main() {
-	input := os.get_lines()
-	cuboids := input.map(parse_cuboid(it))[..20]
-
+fn count_lit_cuboids(cuboids []Cuboid) i64 {
 	mut effective_cubes := []Cuboid{}
 	for cuboid in cuboids {
 		for prev_cuboid in effective_cubes.clone() {
@@ -80,6 +82,14 @@ fn main() {
 		}
 	}
 
-	sum := arrays.sum(effective_cubes.map(it.volume() * it.factor))?
-	println('Lit cubes after first part of init procedure; Part 1: ${sum}')
+	return arrays.sum(effective_cubes.map(it.volume() * it.factor)) or { 0 }
 }
+
+fn main() {
+	input := os.get_lines()
+	cuboids := input.map(parse_cuboid(it))
+
+	println('Lit cubes after first part of init procedure; Part 1: ${count_lit_cuboids(cuboids.clone()[..20])}')
+	println('Lit cubes after entire reboot; Part 2: ${count_lit_cuboids(cuboids.clone())}')
+}
+
